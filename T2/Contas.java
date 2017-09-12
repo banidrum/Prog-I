e112e334523eec0e2f0e341e3fc43dc458d1768b
Daniel Brum <dani3544@gmail.com>
2017-09-12 15:52:35 -0300

    Add files via upload

---
+++ T2/Contas.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Contas{
+       public static void main (String[] args){	
+       Scanner leia = new Scanner(System.in);  
+
+
+       int numero; 
+       String dono;	
+       double saldo;
+       double valor;
+	
+       public void Depositar(){
+	 System.Out.println("Digite quanto quer depositar: ");
+	 valor = leia.nextDouble();
+		}	
+     
+       public void Sacar(){
+
+		}     	
+
+
+	}
+
+}
