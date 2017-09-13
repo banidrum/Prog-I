84757f4cdefbb2336d09d351669883d66943dc3d
Daniel Brum <dani3544@gmail.com>
2017-09-13 16:47:20 -0300

    Update Trab2.java

--- T2/Trab2.java
+++ T2/Trab2.java
@@ -1,6 +1,32 @@
 public class Trab2{
        public static void main(String[] args){
        
+       int op;
+              
+       do{
+              
+       System.out.println("MENU PRINCIPAL");
+       System.out.println("1- Consultar saldo");       
+       System.out.println("2- Efetuar saque");                     
+       System.out.println("3- Efetuar depósito");
+       System.out.println("4- Efetuar transferência");
+       System.out.println("5- Exibir dados da conta");
+       System.out.println("6- Sair");      
+       
+       switch(op){
+              case 1:   
+              break;
+              case 2:
+              break;
+              case 3:
+              break;
+              case 4:
+              break;
+              case 5:
+              break;       
+                  }       
+              
+       }while(op!=6);
        
        
        
