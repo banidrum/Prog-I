b3e39117ef89a6807cd9b6c85f463a29fe36e1f4
Daniel Brum <dani3544@gmail.com>
2017-10-05 20:33:25 -0300

    Add files via upload

---
+++ Publicacao.java
@@ -0,0 +1,15 @@
+class Publicacao {
+
+				private String nome;
+				private double precoExemplar;
+				protected double valorAnuidade;
+
+				public calcularAnuidade(){
+							12 * 	precoExemplar;
+				}
+				
+				public imprimirDados(){
+				
+				
+				}
+}
---
+++ Publicacaow.java
@@ -0,0 +1,17 @@
+public class Publicacaow extends Publicacao{
+		
+			 private int taxaEntrega;
+			 
+			 public void calcularAnuidade(){
+			 				super.precoExemplar * 52;
+			 }	
+	
+			 public void calcularTaxaEntrega(){
+			 
+			 			  taxaEntrega = 0.05 * 52;			 
+			 }
+			 
+			 public void imprimirDados(){
+			 
+			 } 
+	}
