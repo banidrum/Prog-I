0fc461da914d9606ab3b28534f984c59f3ded8ce
Daniel Brum <dani3544@gmail.com>
2017-10-05 19:25:35 -0300

    Update Getset.java

--- Exemplos/Getset.java
+++ Exemplos/Getset.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 public class Getset{
 	   public static void main(String[] args){
 	   
-	   Pessoa p = new Pessoa();
+	   ArrayList <Pessoa> pessoas = new ArrayList();
 	   
-	   List pessoa = new ArrayList();
-	   
-	   pessoas.add(p);
+	   pessoas.add(new Pessoa(nome, peso, idade)); // chama construtor e passa dados
 	   	   
+	   
+	   
+	   
 	   }
 }
